382bf60048b521744deeb5527a7e19ed47539dbc
xream xu <xreamxu@gmail.com>
2011-12-09 05:10:15 +0800

    Speed up Apple services /Tumblr ! No network reset for Apps!

--- README.md
+++ README.md
@@ -5,13 +5,27 @@
 
 ###[Install page](http://xream.github.com/iGV/install.html) updates as well.
 
-12-08
+2011-12-09
+
+Rewrite scripts
+
+OpenVPN support improved.
+
+Speed up **ALL** Apple services! (beta)
+
+Apps like Filpboard won't cause network reset!
+
+Speed up Tumblr!
+
+2011-12-08
+
+Remove hosts
+
+Rewrite ip-up/ip-down
 
 New pages,README
 
 OpenVPN(beta)
-
-use smarthosts to speed up(beta)
 
 Easy to manage configuration files of dnsmasq(beta)
 
@@ -27,7 +41,6 @@ Speed up Google services
 
 #In Process
 
-Rewrite the GitHub page
 
 Update automatically
 
--- gateway.sh
+++ gateway.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/sbin
-#If the ip-up script cannot get the right gateway automatically
-#set the gateway below
-
-#DO NOT change anything in this file if nothing goes wrong!!!
-
-#this is your old gateway
-yourOLDGW=0.0.0.0
-
-#this is your VPN gateway
-yourVPNGW=0.0.0.0
-
-if [ "${yourOLDGW}" == "0.0.0.0" ] && [ "${yourVPNGW}" == "0.0.0.0" ];then
-	#noneovpn
-	VPNGW=`netstat -nr | grep ^default |grep ppp|awk '{print $2}'`
-	#ovpn tun
-	if [ -z ${VPNGW} ]; then
-		VPNGW=`ifconfig | grep -A 3 tun|grep netmask | awk '{print $4}'`
-	fi
-	#ovpn tap
-	if [ -z ${VPNGW} ]; then
-	    VPNGW=`netstat -nr | grep ^0/1 | grep -P 'tun|tap' | head -1 | awk '{print $2}'`
-	fi
-	#get oldgw	
-    OLDGW=`netstat -nr | grep -P '^default|^0/1' | grep -v $(echo ${VPNGW}) | head -1 | awk '{print $2}'`
+#getgw
+firstgw=`netstat -nr|grep -A1 Gate|egrep -v "Gate|lo"|awk '{print $2}'`
+secondgw=`netstat -nr|grep -A2 $firstgw|egrep -v "$firstgw|lo"|awk '{print $2}'|head -1`
+netif=`netstat -nr|grep -A1 Netif|egrep -v "Netif|lo"|awk '{print $6}'`
+if [[ $netif == e* ]]; then
+	oldgw=$firstgw
+	vpngw=$secondgw
 else
-	OLDGW="${yourOLDGW}"
-	VPNGW="${yourVPNGW}"
+	oldgw=$secondgw
+	vpngw=$firstgw	
 fi
-
-	
+#set the gateway below
+#DO NOT uncomment them if nothing goes wrong!!!
+#oldgw=192.168.2.1
+#vpngw=192.168.0.1
