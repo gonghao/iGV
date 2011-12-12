4adc9d0afd30989f1449dd1b7a114b4d2b541e1c
xream xu <xreamxu@gmail.com>
2011-12-12 19:35:36 +0800

    add cydiahelp.com

--- gateway.sh
+++ gateway.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/sbin
 #getgw
-firstgw=`netstat -nr|grep -A1 Gate|egrep -v "Gate|lo|link"|awk '{print $2}'`
-secondgw=`netstat -nr|grep -A2 $firstgw|egrep -v "$firstgw|lo"|awk '{print $2}'|head -1`
-netif=`netstat -nr|grep -A1 Netif|egrep -v "Netif|lo|link"|awk '{print $6}'`
-if [[ $netif == e* ]]; then
-	oldgw=$firstgw
-	vpngw=$secondgw
-else
-	oldgw=$secondgw
-	vpngw=$firstgw	
-fi
+vpngw=`ifconfig |egrep -A1 "tun|tap|ppp"|tail -1|awk '{print $4}'`
+oldgw=`netstat -nr|egrep "default.*en"|awk '{print $2}'`
 #set the gateway below
 #DO NOT uncomment them if nothing goes wrong!!!
 #oldgw=192.168.2.1
